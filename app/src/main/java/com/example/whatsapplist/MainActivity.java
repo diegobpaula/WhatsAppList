7530a6bba5657bd84cf3cbc530f9dd7873a4b428
diegobpaula <dih.ferratone@gmail.com>
2022-08-08 21:19:59 -0400

    event click

--- app/src/main/java/com/example/whatsapplist/MainActivity.java
+++ app/src/main/java/com/example/whatsapplist/MainActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import com.example.whatsapplist.Adapter.AdapterUsuario;
 import com.example.whatsapplist.Model.Usuario;
+import com.example.whatsapplist.RecyclerItemClickListener.RecyclerItemClickListener;
 import com.example.whatsapplist.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
@@ -31,9 +35,33 @@ public class MainActivity extends AppCompatActivity {
 
         Usuarios();
 
+        // Evento de Click na RecyclerView
+        binding.recyclerUsuario.addOnItemTouchListener(new RecyclerItemClickListener(
+                getApplicationContext(),
+                binding.recyclerUsuario,
+                new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+
+                        Usuario usuario = usuarioList.get(position);
+
+                        Toast.makeText(MainActivity.this, "Item foi clicado " + usuario.getNome(), Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onLongItemClick(View view, int position) {
+
+                    }
+
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                    }
+                }));
+
     }
 
-    public void Usuarios(){
+    public void Usuarios() {
         Usuario usuario1 = new Usuario(R.drawable.usuario1, "Marcos", "Olá como vai?");
         usuarioList.add(usuario1);
 
